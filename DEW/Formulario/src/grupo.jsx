e1dd4c5ba08e4cb023f95e54d0963f7bc40b5049
2dawa <2dawa@informatica.edu>
2025-01-08 12:21:06 +0000

    commit 08-01-25

--- DEW/Formulario/src/grupo.jsx
+++ DEW/Formulario/src/grupo.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-let alumnos = [
+const alumnos = [
     {id: 1, grupo: 'A', nombre: "Juan"},
     {id: 2, grupo: 'A', nombre: "Eva"},
     {id: 3, grupo: 'B', nombre: "Ana"},
     {id: 4, grupo: 'B', nombre: "Julia"},
     {id: 5, grupo: 'B', nombre: "Antonio"},
 ]
+const grupos = ['A','B']
 export default function Grupo(){
     const {letra} = useParams();
     return(
@@ -20,12 +21,67 @@ export default function Grupo(){
                     )
                 }
             </ul>
-            <form action="" id="formAlumnos">
-                <label htmlFor="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre" />
-                <br />
-                <input type="submit" value="Añadir alumno"/>
-            </form>
+            <div>
+                <form action="" id="addAlumnos">
+                    <h3>Añadir Alumno</h3>
+                    <div>
+                        <label htmlFor="id">ID: </label>
+                        <input type="text" name="id" id="id" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="nombre">Nombre: </label>
+                        <input type="text" name="nombre" id="nombre" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="grupo">Grupo: </label>
+                        <select name="grupo" id="grupo" required>
+                            { grupos.map( (grupo,i) => <option key={`${i}+${grupo}`} value={grupo}>{grupo}</option>)}
+                        </select>
+                    </div>
+                    <input type="submit" value="Añadir alumno"/>
+                </form>
+            </div>
+            <div>
+                <form action="" id="deleteAlumnos">
+                    <h3>Borrar Alumno</h3>
+                    <div>
+                        <label htmlFor="id">ID: </label>
+                        <input type="text" name="id" id="id" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="nombre">Nombre: </label>
+                        <input type="text" name="nombre" id="nombre" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="grupo">Grupo: </label>
+                        <select name="grupo" id="grupo" required>
+                            { grupos.map( (grupo,i) => <option key={`${i}+${grupo}`} value={grupo}>{grupo}</option>)}
+                        </select>
+                    </div>
+                    <input type="submit" value="Borrar alumno"/>
+                </form>
+            </div>
+            <div>
+                <form action="" id="editAlumnos">
+                    <h3>Editar datos del Alumno</h3>
+                    <div>
+                        <label htmlFor="id">ID: </label>
+                        <input type="text" name="id" id="id" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="nombre">Nombre: </label>
+                        <input type="text" name="nombre" id="nombre" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="grupo">Grupo: </label>
+                        <select name="grupo" id="grupo" required>
+                            { grupos.map( (grupo,i) => <option key={`${i}+${grupo}`} value={grupo}>{grupo}</option>)}
+                        </select>
+                    </div>
+                    <input type="submit" value="Actualizar alumno"/>
+                </form>
+            </div>
+            
         </>
     )
 }
